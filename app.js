d25f3228ea2b5063e411782dc0e2f86267f8de88
EC2 Default User <ec2-user@ip-172-31-37-224.us-east-2.compute.internal>
2020-07-31 05:03:57 +0000

    updated

--- app.js
+++ app.js
@@ -46,6 +46,30 @@ app.get("/api/updateFavorites", function(req, res){
     
 });//api/updateFavorites
 
+app.get("/getKeywords", function(req, res) 
+   {
+    let sql = "SELECT DISTINCT keyword FROM favorites ORDER BY keyword";
+    let imageUrlArray = ["img/favorite.png"];
+    pool.query(sql, function(err, rows, fields) {
+        if (err) throw err;
+        console.log(rows);
+        res.render("favorites", { "imageUrlArray": imageUrlArray, "rows": rows })
+    });
+});//getKeywords
+
+app.get("/api/getFavorites", function(req, res)
+  {
+  let sql = "SELECT imageURL FROM favorites WHERE keyword = ?";
+  let sqlParams = [req.query.keyword];  
+  pool.query(sql, sqlParams, function (err, rows, fields) {
+
+    if (err) throw err;
+    console.log(rows);
+    res.send(rows);
+  });
+    
+});//api/getFavorites
+
 
 async function getRandomImage(keyword, count) {
     
