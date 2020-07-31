3b9a92bbf3e7a5f16c56bdf7a1a8ce1563b303d6
EC2 Default User <ec2-user@ip-172-31-37-224.us-east-2.compute.internal>
2020-07-31 01:25:56 +0000

    updated

--- public/js/functions.js
+++ public/js/functions.js
@@ -5,13 +5,38 @@ $(document).ready(function() {
     
     $(".favoriteIcon").on("click", function() { 
         
+        let queryString = window.location.search;
+        let urlParams   = new URLSearchParams(queryString);
+        let keyword     = urlParams.get("keyword");
+        
+        let imageUrl = $(this).prev().attr("src");
+        
         if ($(this).attr("src") == "img/favorite.png") {
             $(this).attr("src", "img/favorite_on.png");
+            updateFavorite("add", imageUrl, keyword);
         }
         else {
             $(this).attr("src", "img/favorite_on.png");
+            updateFavorite("add", imageUrl);
         }
         
         });
+
+
+function updateFavorite(action, imageUrl, keyword) {
+    $.ajax({
+        method: "get",
+        url: "/api/updateFavorites",
+        data: {
+            "action": action,
+            "imageUrl": imageUrl,
+            "keyword": keyword
+        },
+        success: function(data, status) {
+            
+        }
+    });
+}
+
 });
         
