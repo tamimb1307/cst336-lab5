ad9c29c1167cea8401afbce1bad6d9e0c6fd1c8d
EC2 Default User <ec2-user@ip-172-31-37-224.us-east-2.compute.internal>
2020-07-31 00:01:40 +0000

    updated

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
 const express = require("express");
 const app = express();
+const request = require("request");
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 //routes
-app.get("/", function(req, res){
+app.get("/", async function(req, res){
     
-    res.render("index");
-    
+    let imageUrlArray = await getRandomImage("", 1);
+    res.render("index", {"imageUrlArray": imageUrlArray});
+
 });
+
+//-search route
+app.get("/search", async function(req, res){
+    
+    let keyword = "";
+    if (req.query.keyword) {
+        keyword = req.query.keyword;
+    }
+    
+    let imageUrlArray = await getRandomImage(keyword, 9);
+    res.render("results", {"imageUrlArray": imageUrlArray});
+
+});
+
+async function getRandomImage(keyword, count) {
+    
+    return new Promise (function (resolve, reject){
+    let requestUrl = `https://api.unsplash.com/photos/random/?count=${count}&client_id=t1NKuNnQ3gnELf2v3vVr9TREGipCoIEM9pRV3RiJmYI&featured=true&orientation=landscape&query=${keyword}`;
+    
+    request(requestUrl, function (error, response, body) {
+        if (!error && response.statusCode == 200){
+            let parsedData = JSON.parse(body);
+            
+            let imageUrlArray = [];
+            for(let i=0; i < count; i++){
+                imageUrlArray.push(parsedData[i]["urls"]["regular"]);
+                
+            }
+            
+            resolve (imageUrlArray);
+        }
+        
+        else {
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode)
+            reject(error)
+        }
+    });
+    });
+}
+
 
 //starting server
 app.listen(process.env.PORT, process.env.IP, function(){
---
+++ public/js/functions.js
@@ -0,0 +1,17 @@
+/* global $ */
+$(document).ready(function() {
+    
+    /*event when clicking icon fills*/
+    
+    $(".favoriteIcon").on("click", function() { 
+        
+        if ($(this).attr("src") == "img/favorite.png") {
+            $(this).attr("src", "img/favorite_on.png");
+        }
+        else {
+            $(this).attr("src", "img/favorite.png");
+        }
+        
+        });
+});
+        
